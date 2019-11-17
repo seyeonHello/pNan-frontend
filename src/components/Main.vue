1fc0d73fffbdca197a47caaacd7248b14f0282c2
Koo minsoo <rnasltn@ajou.ac.kr>
2019-11-17 17:29:40 +0900

    Build Mainpage/header/navigation & Environment Setting(Docker, Axios, Router)

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,38 +3,42 @@
         <div class="container-fluid">
             <div class="row flex-nowrap">
                 <div class="col-2">
-                    <ul class="nav flex-column">
-                        <li class="nav-item mt-2 mb-4">
-                            <router-link :to="{ path: '/'}" class="navbar-brand nav-link">
-                                <img src="../assets/logo.png">
-                            </router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link :to="{ path: '/VIC'}" class="nav-link">
-                                VIC 작성
-                            </router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link :to="{ path: '/followUp'}" class="nav-link">
-                                팔로우업 일지 작성
-                            </router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link :to="{ path: '/visit'}" class="nav-link">
-                                방문 일지 작성
-                            </router-link>
-                        </li>
-                        <li>
-                            <router-link :to="{ path: '/statistic'}" class="nav-link">
-                                통계
-                            </router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link :to="{ path: '/dataRoom'}" class="nav-link">
-                                자료실
-                            </router-link>
-                        </li>
-                    </ul>
+                    <nav class="navbar" role="navigation">
+                        <div class="collapse navbar-collapse navbar-ex1-collapse">
+                            <ul class="nav navbar-nav">
+                                <li class="nav-item mt-2 mb-4">
+                                    <router-link :to="{ path: '/'}" class="navbar-brand button">
+                                        <img src="../assets/logo.png">
+                                    </router-link>
+                                </li>
+                                <li class="nav-item">
+                                    <router-link :to="{ path: '/VIC'}" class="button">
+                                        VIC 작성
+                                    </router-link>
+                                </li>
+                                <li class="nav-item">
+                                    <router-link :to="{ path: '/followUp'}" class="button">
+                                        팔로우업 일지 작성
+                                    </router-link>
+                                </li>
+                                <li class="nav-item">
+                                    <router-link :to="{ path: '/visit'}" class="button">
+                                        방문 일지 작성
+                                    </router-link>
+                                </li>
+                                <li>
+                                    <router-link :to="{ path: '/statistic'}" class="button">
+                                        통계
+                                    </router-link>
+                                </li>
+                                <li class="nav-item">
+                                    <router-link :to="{ path: '/dataRoom'}" class="button">
+                                        자료실
+                                    </router-link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
                 </div>
                 <div class="col-10">
                     <div class="page-content">

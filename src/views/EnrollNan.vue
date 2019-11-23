fe84639cda274ceba366cb1a6ecb974b22bea358
seyeon <rlatpdus321@ajou.ac.kr>
2019-11-23 23:26:25 +0900

    enrollNan resize

--- src/views/EnrollNan.vue
+++ src/views/EnrollNan.vue
@@ -1,21 +1,23 @@
 <template>
   <v-app style="background: #FFFF;">
-    <div id="enroll">
-      <v-form id="enrollNan">
-        <v-container>
-          <h2>등록</h2>
-          <v-layout column>
-            <v-flex>
-              <v-text-field label="이름" v-model="name"></v-text-field>
-              <v-text-field label="국적" v-model="area"></v-text-field>
-              <v-text-field label="생년월일(6자리)" v-model="birth"></v-text-field>
-              <v-textarea label="status" v-model="status"></v-textarea>
-              <v-btn dark color="indigo" @click="submit">submit</v-btn>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-form>
-    </div>
+    <v-row justify="center">
+      <div id="enroll">
+        <v-form id="enrollNan">
+          <v-container>
+            <h2>난민 등록</h2>
+            <v-layout column>
+              <v-flex>
+                <v-text-field label="이름" v-model="name"></v-text-field>
+                <v-text-field label="국적" v-model="area"></v-text-field>
+                <v-text-field label="생년월일(yyyy-mm-dd)" v-model="birth"></v-text-field>
+                <v-textarea label="status" v-model="status"></v-textarea>
+                <v-btn dark color="indigo" @click="submit">submit</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-form>
+      </div>
+    </v-row>
   </v-app>
 </template>
 
@@ -43,10 +45,11 @@ export default {
     text-align: center;
   }
 
-  #enroll2{
-    text-align:center;
-    display: inline-block;
-    vertical-align: middle;
+  #enroll{
+    position: absolute;
+    text-align: center;
+    vertical-align: center;
+    horiz-align: center;
     width:500px;
     margin:auto;
   }

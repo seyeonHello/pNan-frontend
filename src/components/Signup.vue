83d104bff787d0fb8535a9c4a9d1f1890177721e
seyeon <rlatpdus321@ajou.ac.kr>
2019-11-23 23:19:26 +0900

    signup resize

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,22 +1,24 @@
 <template>
   <v-app style="background: #FFFF;">
-    <div id="signup">
-      <v-form id="signup2">
-        <v-container>
-          <h2>등록</h2>
-          <v-layout column>
-            <v-flex>
-              <v-text-field label="이름" v-model="name"></v-text-field>
-              <v-text-field label="생년월일" v-model="birth"></v-text-field>
-              <v-text-field label="ID" v-model="id"></v-text-field>
-              <v-text-field label="password" v-model="password"></v-text-field>
-              <v-btn dark color="indigo darken-2" @click="submit">save</v-btn>
-              <v-btn id="exitbtn" dark color="indigo darken-2" @click="exit">exit</v-btn>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-form>
-    </div>
+    <v-row justify="center">
+      <div id="signup">
+        <v-form id="signup2">
+          <v-container>
+            <h2>회원가입</h2>
+            <v-layout column>
+              <v-flex>
+                <v-text-field label="이름" v-model="name"></v-text-field>
+                <v-text-field label="생년월일" v-model="birth"></v-text-field>
+                <v-text-field label="ID" v-model="id"></v-text-field>
+                <v-text-field label="password" v-model="password"></v-text-field>
+                <v-btn dark color="indigo darken-2" @click="submit">save</v-btn>
+                <v-btn id="exitbtn" dark color="indigo darken-2" @click="exit">exit</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-form>
+      </div>
+    </v-row>
   </v-app>
 </template>
 
@@ -44,9 +46,11 @@ export default {
   *{
     text-align: center;
   }
-  #sign{
+  #signup{
     position: absolute;
     text-align: center;
+    vertical-align: center;
+    horiz-align: center;
     width:500px;
     margin:auto;
   }

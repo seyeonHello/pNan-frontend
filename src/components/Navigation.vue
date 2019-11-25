756d220d0c6ff8fc9f39f1b69178ca8d3c28c844
seyeon <rlatpdus321@ajou.ac.kr>
2019-11-25 22:46:31 +0900

    navigation route change

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,58 +1,60 @@
 <template>
+  <v-app style="background: #FFFF;">
     <div class="main">
-        <div class="container-fluid">
-            <div class="row flex-nowrap">
-                <div class="col-2">
-                    <nav class="navbar nav-pills" role="navigation">
-                        <div class="collapse navbar-collapse navbar-ex1-collapse">
-                            <ul class="nav navbar-nav">
-                                <!--
-                                <li class="nav-item mt-2 mb-4">
-                                    <router-link :to="{ path: '/'}" class="navbar-brand button">
-                                        <img src="../assets/logo.png">
-                                    </router-link>
-                                </li>
-                                -->
-                                <li class="nav-item">
-                                    <router-link :to="{ path: '/login'}" class="button">
-                                        로그인
-                                    </router-link>
-                                </li>
-                                <li class="nav-item">
-                                    <router-link :to="{ path: '/enrollNan'}" class="button">
-                                        난민 등록
-                                    </router-link>
-                                </li>
-                                <li class="nav-item">
-                                    <router-link :to="{ path: '/showRefugee'}" class="button">
-                                        난민 리스트
-                                    </router-link>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <router-link :to="{ path: '/followUp'}" class="button">
-                                        방문일지 작성
-                                    </router-link>
-                                    <router-link :to="{ path: '/followUp'}" class="button">
-                                        방문일지 확인
-                                    </router-link>
-                                </li>
-                                <li class="nav-item">
-                                    <router-link :to="{ path: '/visit'}" class="button">
-                                        통계
-                                    </router-link>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
-                <div class="col-10">
-                    <div class="page-content">
-                        <router-view/>
-                    </div>
-                </div>
+      <div class="container-fluid">
+        <div class="row flex-nowrap">
+          <div class="col-2">
+            <nav class="navbar nav-pills" role="navigation">
+              <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                  <!--
+                  <li class="nav-item mt-2 mb-4">
+                      <router-link :to="{ path: '/'}" class="navbar-brand button">
+                          <img src="../assets/logo.png">
+                      </router-link>
+                  </li>
+                  -->
+                  <li class="nav-item">
+                    <router-link :to="{ path: '/login'}" class="button">
+                      로그인
+                    </router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link :to="{ path: '/enrollNan'}" class="button">
+                      난민 등록
+                    </router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link :to="{ path: '/showRefugee'}" class="button">
+                      난민 리스트
+                    </router-link>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <router-link :to="{ path: '/writeVisit'}" class="button">
+                      방문일지 작성
+                    </router-link>
+                    <router-link :to="{ path: '/showVisit'}" class="button">
+                      방문일지 확인
+                    </router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link :to="{ path: '/statistic'}" class="button">
+                      통계
+                    </router-link>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+          <div class="col-10">
+            <div class="page-content">
+              <router-view/>
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  </v-app>
 </template>
 
 <!--suppress JSUnusedGlobalSymbols -->
